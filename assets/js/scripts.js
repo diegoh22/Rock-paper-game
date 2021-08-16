47682feb1fe9d8e3a0ad235635e24dff4a1ab22f
diegoh22 <diegohofficial@gmail.com>
2021-08-16 20:57:03 +0000

    add more functions now you can win

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,25 +1,59 @@
 const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 
+const scoreEL = document.getElementById('score');
 
+const userWinner = ['scissorspaper', 'paperrock','rocklizard','lizardspock','spockscissors',
+
+'rockscissors', 'scissorslizard','lizardpaper','paperspock','spockrock']
+
+let score = ('0')
+let userchoice = '';
+let pickRandomChoice = '';
 
 window.addEventListener('load', () => {
-
-let userchoice = '';
 
 document.querySelectorAll('.user-choice .game-choice').forEach(card => {
 
 card.addEventListener('click',(ev) => {
 
 userChoice = ev.target.dataset.choice
+pickRandomChoice = getpickRandomChoice();
 
-
+ starGame();
 })
 
 })
 
 })
 
+function starGame() {
 
-function pickRandomChoice() {
+   findWinner('userchoice, pickRandomChoice');
+}
+
+function findWinner(user, comp) {
+    if(user === comp) {
+        console.log('tie');
+    }
+    else if (getuserWinner(user + comp)) {
+        console.log('you Lose');
+    } else{
+     
+        console.log('You Win');
+
+    }
+}
+function getuserWinner(result){
+    return userWinner.some(Winstr => Winstr === result);
+}
+
+
+
+function updateScore(value) {
+    score += value;
+    scoreEL.innerText = score;
+}
+
+function getpickRandomChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
