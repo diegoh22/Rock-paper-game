9701abb148260c243091eeedb105dbc22d12a708
diegoh22 <diegohofficial@gmail.com>
2021-08-16 19:39:36 +0000

    add eventlistening to the game

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,20 @@
-window.addEventListener('load', () => {
-let userchoice = '';
-document.querySelectorAll('.user-choice .game-choice').forEach(card => {
-card.addEventListener('click',(ev) => {
-userChoice = getUserChoice(ev.target)
-startGame();
-}
-)
-})
-})
-function startGame(){
-}
-function getUserChoice(target) {
-console.log(target);
-}
 
-function getUserChoice(target) {
-console.log(target);
-if (target.nodeName === "IFRAME") {
-return target.parentElement.classList[1];
-}
-return target.classList[1];
-}
+
+window.addEventListener('load', () => {
+
+let userchoice = '';
+
+document.querySelectorAll('.user-choice .game-choice').forEach(card => {
+
+card.addEventListener('click',(ev) => {
+
+userChoice = ev.target.dataset.choice
+
+
+})
+
+})
+
+})
+
+
